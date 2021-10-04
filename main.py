ec905b4e5dc54a9b700e2b02bbee2802d58ecfef
ram <ramrsankar11@gmail.com>
2021-10-03 20:06:01 -0400

    Refactoring.

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ app = Flask(__name__)
 api = Api(app)
 
 
-@api.route("/predict")
+@api.route("/get_prediction")
 class Predict(Resource):
     def post(self):
-        # Todo: Link prediction.
-        return {
-            "result": {
-                "ram": "absent",
-                "jerin": "present",
-                "praharsh": "absent",
-                "prithvi": "present",
-                "abhishek": "absent",
-            },
-            "message": "Success",
-        }, 200
+        try:
+            return {
+                "result": {
+                    "ram": "absent",
+                    "jerin": "present",
+                    "praharsh": "absent",
+                    "prithvi": "present",
+                    "abhishek": "absent",
+                },
+                "message": "Success",
+            }, 200
+        except Exception as err:
+            return {
+                "message": "Error",
+            }, 500
 
 
 if __name__ == "__main__":
