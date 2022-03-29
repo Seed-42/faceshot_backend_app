0c4ea5933c316035242397888f8e927d0e2bf66d
ram <ramrsankar11@gmail.com>
2022-03-29 17:06:04 -0400

    Face training and Embeddings backup in cloud feature added.

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,14 +4,14 @@ import time
 import uuid
 from threading import Thread
 
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 from flask_restx import Resource
 
 from actions.FaceRec import FaceRec
-from actions.train import TrainFaceRecognizer
 from api import api_blueprint, api
-from utils import utils
+from utils import utils, gcloud_utils
 from config import config
 
 app = Flask(__name__)
@@ -19,23 +19,75 @@ CORS(app)
 app.register_blueprint(api_blueprint)
 
 
+@api.route("/reset_embeddings")
+class ResetEmbeddings(Resource):
+    def post(self):
+        try:
+            # empty embeddings file.
+            utils.reset_embeddings()
+
+            # Backup existing embeddings and upload new embeddings to cloud.
+            gcloud_utils.move_existing_embedding_to_backup()
+            gcloud_utils.upload_new_embeddings()
+
+            return {
+                       "message": "Resetting embeddings complete",
+                       "success": "true"
+            }, 200
+
+        except Exception as err:
+            return {
+                       "message": "Error",
+                       "success": "false"
+                   }, 500
+
+
 @api.route("/train")
 class TrainFaceRec(Resource):
     def post(self):
 
         try:
-            def do_work(value):
-                time.sleep(value)
-                # Todo: Add facial features and save embeddings.
-                TrainFaceRecognizer().train()
-                # Todo: Upload new embeddings and add current to backup in GS.
-                # Todo: Send a post to firebase to notify training is done.
+            # def do_work(value):
 
-            thread = Thread(target=do_work, kwargs={'value': request.args.get('value', 20)})
-            thread.start()
+            value = request.get_json()
+
+            # Create a data folder to store tmp files for current request.
+            tmp_data_folder_path = os.path.join(config.APP_TEMP_PATH, str(uuid.uuid4()))
+            if os.path.exists(tmp_data_folder_path):
+                shutil.rmtree(tmp_data_folder_path)
+            os.makedirs(tmp_data_folder_path)
+
+            url = value.get("url", "")
+            object_id = value.get("id", "")
+
+            if not all([url, id]):
+                raise AttributeError("Missing attributes in input request.")
+
+            # Download image from url and load it.
+            image_np = utils.download_and_load_image(url, tmp_data_folder_path)
+
+            # Add facial features and save embeddings.
+            FaceRec().train(image_np, object_id)
+
+            # Save embeddings
+            utils.save_embeddings()
+
+            # Upload new embeddings and add current file to back up folder in GS.
+            gcloud_utils.move_existing_embedding_to_backup()
+            gcloud_utils.upload_new_embeddings()
+
+            # Send a post to firebase to notify training is done.
+            # requests.post()
+
+            # Remove tmp data folder.
+            if os.path.exists(tmp_data_folder_path):
+                shutil.rmtree(tmp_data_folder_path)
+
+            # thread = Thread(target=do_work, kwargs={'value': request.get_json()})
+            # thread.start()
 
             return {
-                "message": "Train Accepted.",
+                "message": "Training complete.",
                 "success": "true"
             }, 200
 
@@ -53,7 +105,7 @@ class TestPredict(Resource):
             return {
                 "result": [
                     {
-                        "student_id": "1a2a34s5d23f4d",
+                        "student_id": "monica@gmail.com",
                         "student_attendance_confidence": 99.0,
                         "file_url": "https://storage.googleapis.com/seed42-faceshot-phase2-output-images/2d81bd1b-d6fb-4fed-81e8-b3eaf8088791/69ce3b5f-da9b-42c3-9ea5-ec40deac7e70.jpeg"
                     },
